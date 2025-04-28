d14f37019de5e01faac1210f1f50de340c1032a4
Ansh Malhotra <83686079+ansh-Bytes217@users.noreply.github.com>
2025-04-28 08:36:44 +0530

    Update Home.jsx

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,8 +1,23 @@
-import blogs from '../data/blogs';
+import { useState, useEffect } from 'react';
 import BlogCard from '../components/BlogCard';
+import axios from 'axios';
 
 function Home() {
-  
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/blogs');
+        setBlogs(response.data);
+      } catch (error) {
+        console.error('Error fetching blogs:', error);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
   return (
     <div className="container mt-4">
       <div className="row">
